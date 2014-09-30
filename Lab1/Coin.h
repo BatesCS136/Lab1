91b34759cc62eabd5a4fbd20d5c44be30ea7b1c3
Garrett Bates <BatesCS136@gmail.com>
2014-09-29 20:31:47 -0700

    continued the assignment and added some documentation

--- Lab1/Coin.h
+++ Lab1/Coin.h
@@ -1,6 +1,12 @@
 #ifndef COIN_H
 #define COIN_H
 
+/*
+// Each Coin structure is given a denomination, quantity,
+// and two labels for use when the quantity is greater
+// than one.
+*/
+
 struct Coin {
 	int denom;
 	long count;
--- Lab1/Main.cpp
+++ Lab1/Main.cpp
@@ -1,36 +1,120 @@
+/*
+// Author: Garrett Bates
+// Date: Sept 29, 2014
+// Lab 1: Show me the MONEY!
+*/
+
 #include <iostream>
 #include "Coin.h"
 
 using namespace std;
 
+/*
+// Function prototypes
+*/
+
 void initCoins (Coin iCoins []);
-void menu();
+void Menu(Coin iCoins[]);
+void Change(long &iAmount, Coin &iCoin);
+const void Print(Coin &iCoin);
 
 int main() {
 
-	Coin coins[4];
+	/*
+	// Declare and initialize the array of four Coin structures
+	*/
 
-	menu();
+	Coin coins[4];
+	initCoins(coins);
+
+	Menu(coins);
 
 	return 0;
 }
 
 void initCoins(Coin iCoins []) {
-	iCoins[0] = { 25, 0, 'a', 'b' };
-	iCoins[1] = { 10, 0, 'a', 'b' };
-	iCoins[2] = { 5, 0, 'a', 'b' };
-	iCoins[3] = { 1, 0, 'a', 'b' };
+
+	/*
+	// Each of the Coins is given a denomination, initial quantity of zero,
+	// and a label that is used depending on whether the quantity of that
+	// Coin is one or greater. e.g. If the amount of change returned involves
+	// only one quarter, the label "quarter" will be used, and not "quarters."
+	*/
+
+	iCoins[0] = Coin{ 25, 0, "quarter", "quarters" };
+	iCoins[1] = Coin{ 10, 0, "dime", "dimes" };
+	iCoins[2] = Coin{ 5, 0, "nickel", "nickels" };
+	iCoins[3] = Coin{ 1, 0, "penny", "pennies" };
 }
 
-void menu() {
-	int amount = 0;
+void Menu(Coin iCoins[]) {
 
-	while (amount != -1) {
-		cout << "Enter money amount: (e.g. 145 = $1.45, -1 to quit)" << endl;
+	/* 
+	// Here  the user is asked to enter the amount of change
+	// to be returned. The integer value will be stored in 'amount.'
+	// A value of -1 will cause the program to terminate.
+	*/
+
+	long amount = 0;
+
+	while (true) {
+		cout << "Enter money amount (e.g. 145 = $1.45, -1 to quit): ";
 		cin >> amount;
+
+		if (amount == -1) {
+			cout << "\n\nGood day to you!" << endl;
+			exit;
+		}
+
+		/*
+		// If the amount is greater than or equal to one, then the
+		// amount and the array of Coins will be passed to the 
+		// Change function where the appropriate quantity of coins
+		// of each denomination will be determined and passed to
+		// the Print function for possible display.
+		*/
 
 		if (amount >= 1) {
 
+			cout << "\n\nYour change:" << endl;
+
+			for (int i = 0; i < 4; ++i) {
+				Change(amount, iCoins[i]);
+				Print(iCoins[i]);
+			}
+
+			cout << endl;
 		}
+	}
+}
+
+void Change(long &iAmount, Coin &iCoin) {
+
+	/*
+	// Coin quantities are determined through simple division.
+	// To find the remaining change, we subtract the amount
+	// by the value of the quantity and denomination of that
+	// particular Coin.
+	*/
+
+	iCoin.count = iAmount / iCoin.denom;
+	iAmount -= iCoin.count * iCoin.denom;
+}
+
+const void Print(Coin &iCoin) {
+
+	/*
+	// Here the label and quantity of each Coin passed is
+	// printed out, provided that its quantity is greater
+	// than zero. If the quantity is greater than one, then
+	// the plural label is chosen, otherwise the singular
+	// form is chosen.
+	*/
+
+	if (iCoin.count > 1) {
+		cout << iCoin.multiple << ": " << iCoin.count << endl;
+	}
+	else if (iCoin.count == 1) {
+		cout << iCoin.single << ": " << iCoin.count << endl;
 	}
 }
